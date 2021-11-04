9a1b138b1dda935e3a7933d9cabb2eba771194d3
Greg <gbrewton1980@gmail.com>
2021-11-03 18:17:46 -0600

    Connects MongoDB

--- api_router.js
+++ api_router.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const apiRouter = express.Router();
 const { users } = require("./data");
-const {v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require("uuid");
+const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
+
+// mongo config
+const mongoDB =
+  "mongodb+srv://greg:GWv1lKty2hArudjx@cluster0.wsuqu.mongodb.net/node-api4-project?retryWrites=true&w=majority";
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 apiRouter.use(express.json());
 
@@ -21,17 +31,18 @@ apiRouter.get("/users", (req, res) => {
 });
 
 apiRouter.post("/register", (req, res) => {
-    const { username, password } = req.body;
-    if (!username || !password) {
-        res.status(400).send("username and password are required");
-    }
-    const newUser = {
-        id: uuidv4(),
-        username,
-        password,
-    };
-    users.push(newUser);
-    res.status(201).send(newUser);
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(400).send("username and password are required");
+  }
+
+  const newUser = {
+    id: uuidv4(),
+    username,
+    password,
+  };
+  users.push(newUser);
+  res.status(201).send(newUser);
 });
 
 module.exports = apiRouter;
